fd6fefa405da4c5ff72a9c151fab45f4cd4c14d8
Neenu Somy <neenu.somy@experionglobal.com>
2020-11-23 12:24:31 +0530

    address_card new page dropdown

--- lib/screens/address_book.dart
+++ lib/screens/address_book.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login_demo/widgets/containers/boxes.dart';
+import 'package:login_demo/widgets/containers/common_button.dart';
+import 'package:login_demo/widgets/controls/address_card.dart';
+import 'package:login_demo/widgets/controls/inputs.dart';
+import '../widgets/containers/mainContainer.dart';
 import 'package:login_demo/dropdown_list.dart';
-import 'package:login_demo/widgets/containers/mainContainer.dart';
 
 const kBottomContainerHght = 60.0;
 const kBottomColor = Colors.blue;
@@ -9,147 +14,172 @@ const kTextStyleLarge = TextStyle(fontSize: 15.0, color: Colors.white70);
 class AddressScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: MainContainer(
-        backgroundImage: AssetImage('images/blue_background.jpg'),
-        mainHeading: 'Address Book',
-        bodyContent: AddressBody(text: 'Address', textColor: Colors.black),
-      ),
-    );
-  }
-}
-
-class BottomButton extends StatelessWidget {
-  BottomButton({@required this.onTap, @required this.buttonTitle});
-  final Function onTap;
-  final String buttonTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Center(
-          child: Text(
-            buttonTitle,
-            style: kTextStyleLarge,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MainContainer(
+          subHeading: '',
+          mainHeading: 'Address Book',
+          bodyContent: ListView(
+            children: [
+              entryField(
+                'Address',
+                'Address 1',
+              ),
+              spacing(10.0, 10.0),
+              entryField(
+                'City',
+                'City',
+              ),
+              spacing(10.0, 10.0),
+              entryField(
+                'State',
+                'Kerala',
+                suffixIcon: Icons.keyboard_arrow_down,
+              ),
+              spacing(10.0, 10.0),
+              DropdownMenu(),
+              entryField(
+                'Country',
+                'India',
+                suffixIcon: Icons.keyboard_arrow_down,
+              ),
+              spacing(10.0, 10.0),
+              entryField(
+                'PIN Code',
+                '678976',
+              ),
+              spacing(10.0, 10.0),
+              checkbox(
+                text: 'Make it my Default Delivery Address',
+                textColor: Colors.grey[400],
+                checkColor: Colors.black54,
+                checkActiveColor: Colors.white12,
+              ),
+              BottomButton(
+                buttonTitle: 'Add to Address Book',
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute());
+                },
+              ),
+              CardContainer(
+                icon: Icons.edit_outlined,
+                text: 'Current Address Here',
+                head: 'Punnolikunnel House,\n Pathar PO, \n Nilambur',
+                iconColor: Colors.blue,
+              ),
+              CardContainer(
+                icon: Icons.edit_outlined,
+                text: 'Address here',
+                head:
+                    'MG Road, Opposite Medical Trust,\n Sahodaran Ayyappan Rd, \n near itel it service, Kochi Kerala 689876',
+                iconColor: Colors.blue,
+              ),
+            ],
           ),
         ),
-        margin: EdgeInsets.all(10.0),
-        width: double.infinity,
-        height: kBottomContainerHght,
-        //color: kBottomColor,
       ),
     );
   }
 }
 
-class AddressBody extends StatelessWidget {
-  AddressBody({this.textColor, this.text, this.heading});
-  final Color textColor;
-  final String text;
-  final String heading;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Column(
-      children: [
-        Container(
-          padding: EdgeInsets.all(0.0),
-          decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.white54))),
-          child: inputField('First Name', 'First Name'),
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        Container(
-          padding: EdgeInsets.all(0.0),
-          decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.grey[100]))),
-          child: inputField('Last Name', 'Last Name'),
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        Container(
-            padding: EdgeInsets.all(0.0),
-            decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(color: Colors.grey[100]))),
-            child: DropdownMenu()),
-        SizedBox(
-          height: 10.0,
-        ),
-        Container(
-            padding: EdgeInsets.all(0.0),
-            decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(color: Colors.grey[100]))),
-            child: DropdownMenu()),
-        SizedBox(
-          height: 10.0,
-        ),
-        Container(
-          padding: EdgeInsets.all(0.0),
-          decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.grey[100]))),
-          child: inputField('Pin Code', 'Pin Code'),
-        ),
-        checkbox(
-          text: 'Make it my default delivery Address',
-          textColor: Colors.grey[400],
-          checkColor: Colors.black54,
-          checkActiveColor: Colors.white12,
-        ),
-        BottomButton(
-          buttonTitle: 'Add to Address Book',
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute());
-          },
-        ),
-        Card(
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            color: Colors.white,
-            child: ListTile(
-              leading: Icon(
-                Icons.add,
-                color: Colors.green,
-              ),
-              title: Text(
-                'neenusomy20@gmail.com xdjfksdhfkjfe ekrjwerjew rewkjrlkewjrwe rwqkjrhlkwq',
-                style: TextStyle(color: Colors.teal),
-              ),
-            ))
-      ],
-    ));
-  }
-}
-
-Widget inputField(
-  hintText,
-  labelText,
-) {
-  return TextField(
-    decoration: InputDecoration(
-        enabledBorder: const OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.grey, width: 0.0),
-        ),
-        border: const OutlineInputBorder(),
-        labelStyle: new TextStyle(color: Colors.blue),
-        labelText: labelText,
-        hintText: hintText,
-        hintStyle: TextStyle(color: Colors.white54)),
-  );
-}
-
-class DropdownMenu extends StatefulWidget {
-  @override
-  _DropdownMenuState createState() => _DropdownMenuState();
-}
-
+// class AddressBody extends StatelessWidget {
+//   AddressBody({this.textColor, this.text, this.heading});
+//   final Color textColor;
+//   final String text;
+//   final String heading;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: Column(
+//       children: [
+//         Container(
+//           padding: EdgeInsets.all(0.0),
+//           decoration: BoxDecoration(
+//               border: Border(bottom: BorderSide(color: Colors.white54))),
+//           child: inputField('First Name', 'First Name'),
+//         ),
+//         SizedBox(
+//           height: 10.0,
+//         ),
+//         Container(
+//           padding: EdgeInsets.all(0.0),
+//           decoration: BoxDecoration(
+//               border: Border(bottom: BorderSide(color: Colors.grey[100]))),
+//           child: inputField('Last Name', 'Last Name'),
+//         ),
+//         SizedBox(
+//           height: 10.0,
+//         ),
+//         Container(
+//             padding: EdgeInsets.all(0.0),
+//             decoration: BoxDecoration(
+//                 border: Border(bottom: BorderSide(color: Colors.grey[100]))),
+//             child: DropdownMenu()),
+//         SizedBox(
+//           height: 10.0,
+//         ),
+//         Container(
+//             padding: EdgeInsets.all(0.0),
+//             decoration: BoxDecoration(
+//                 border: Border(bottom: BorderSide(color: Colors.grey[100]))),
+//             child: DropdownMenu()),
+//         SizedBox(
+//           height: 10.0,
+//         ),
+//         Container(
+//           padding: EdgeInsets.all(0.0),
+//           decoration: BoxDecoration(
+//               border: Border(bottom: BorderSide(color: Colors.grey[100]))),
+//           child: inputField('Pin Code', 'Pin Code'),
+//         ),
+//         checkbox(
+//           text: 'Make it my default delivery Address',
+//           textColor: Colors.grey[400],
+//           checkColor: Colors.black54,
+//           checkActiveColor: Colors.white12,
+//         ),
+//         BottomButton(
+//           buttonTitle: 'Add to Address Book',
+//           onTap: () {
+//             Navigator.push(context, MaterialPageRoute());
+//           },
+//         ),
+//         Card(
+//             margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+//             color: Colors.white,
+//             child: ListTile(
+//               leading: Icon(
+//                 Icons.add,
+//                 color: Colors.green,
+//               ),
+//               title: Text(
+//                 'neenusomy20@gmail.com xdjfksdhfkjfe ekrjwerjew rewkjrlkewjrwe rwqkjrhlkwq',
+//                 style: TextStyle(color: Colors.teal),
+//               ),
+//             ))
+//       ],
+//     ));
+//   }
+// }
+//
+// Widget inputField(
+//   hintText,
+//   labelText,
+// ) {
+//   return TextField(
+//     decoration: InputDecoration(
+//         enabledBorder: const OutlineInputBorder(
+//           borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+//         ),
+//         border: const OutlineInputBorder(),
+//         labelStyle: new TextStyle(color: Colors.blue),
+//         labelText: labelText,
+//         hintText: hintText,
+//         hintStyle: TextStyle(color: Colors.white54)),
+//   );
+// }
+//
 List<DropdownMenuItem> getDropdownItems() {
   List<DropdownMenuItem<String>> dropdownItems = [];
   for (String item in stateList) {
@@ -160,6 +190,11 @@ List<DropdownMenuItem> getDropdownItems() {
     dropdownItems.add((newItem));
   }
   return dropdownItems;
+}
+
+class DropdownMenu extends StatefulWidget {
+  @override
+  _DropdownMenuState createState() => _DropdownMenuState();
 }
 
 class _DropdownMenuState extends State<DropdownMenu> {
@@ -189,21 +224,4 @@ class _DropdownMenuState extends State<DropdownMenu> {
       ),
     );
   }
-}
-
-Widget checkbox({text, textColor, checkColor, checkActiveColor}) {
-  return Row(
-    children: [
-      Checkbox(
-        value: true,
-        checkColor: checkColor,
-        activeColor: checkActiveColor,
-        onChanged: (bool value) {},
-      ),
-      Text(
-        text,
-        style: TextStyle(color: textColor),
-      ),
-    ],
-  );
 }
